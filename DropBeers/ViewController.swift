41c1d0a0aec7b1d9c8f0cb91c6061c8133273c2b
Egemen Ayhan <egemenayhan@icloud.com>
2020-09-04 23:39:49 +0300

    Initial Commit

---
+++ DropBeers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DropBeers
+//
+//  Created by Egemen Ayhan on 4.09.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
